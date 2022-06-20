990e7d433c61c4661500bdc1c36f0bdb405f4efd
Solemensis <96355888+Solemensis@users.noreply.github.com>
2022-06-20 18:57:01 +0300

    some functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // variable setup for the easier manipulation of html dom
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+const overlay2 = document.querySelector(".overlay2");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
@@ -16,12 +17,39 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 // this has put in a function box for the sake of dry principle
-const closeModal = function () {
+const closeModals = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+
+  formModal.style.opacity = "0";
+  overlay2.style.opacity = "0";
+  overlay2.style.zIndex = "-1";
+  formModal.style.zIndex = "-1";
+  formModal.style.transition = "0.5s";
+
+  document.querySelector(".main-screen").style.filter = "blur(0)";
+  document.querySelector(".main-screen").style.transform = "scale(1)";
+  document.querySelector(".main-screen").style.transition = "0.5s";
 };
 
 // close modal with "dry" functions
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-modal.addEventListener("click", closeModal);
+btnCloseModal.addEventListener("click", closeModals);
+overlay.addEventListener("click", closeModals);
+overlay2.addEventListener("click", closeModals);
+modal.addEventListener("click", closeModals);
+
+// *****************
+
+let button1 = document.querySelector(".button1");
+let formModal = document.querySelector(".form-modal");
+
+button1.addEventListener("click", function () {
+  formModal.style.opacity = "1";
+  overlay2.style.opacity = "1";
+  overlay2.style.zIndex = "500";
+
+  formModal.style.zIndex = "999999999999999";
+  formModal.style.transition = "1s";
+  document.querySelector(".main-screen").style.filter = "blur(3px)";
+  document.querySelector(".main-screen").style.transform = "scale(1.010)";
+});
